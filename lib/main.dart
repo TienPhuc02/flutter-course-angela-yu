06813658f11100d0e691e37581b9477f4e01b024
TienPhuc02 <dtp2352002@gmail.com>
2023-10-18 11:24:18 +0700

    challenge chapter 6

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,50 @@ import 'package:flutter/material.dart';
 //hàm main khởi điểm của ứng dụng
 //flutter cấu trúc code bởi 1 cây widget(tree widget)
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("hello world"),
-      ),
-    ),
-  );
+  runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.amberAccent,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Container(width: 100, color: Colors.blue),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 /*
 flutter nó hoạt động dựa trên widget(tiện ích) theo mô hình tree, ở đây là widget lớn nhất Material App,
 bên trong đó là widget Center chịu trách nhiệm bố trí màn hình giúp đặt 1 cái gì đó ở giữa các widget con (phần tử con) nằm ở giữa, ở đây có 1 widget con là
